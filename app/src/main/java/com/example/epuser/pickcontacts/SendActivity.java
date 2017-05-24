93d3e15fe60b6fb4a6867f41461d7747c53b8b51
epuser <bittu.dakshana15@gmail.com>
2017-05-24 17:27:44 +0530

    updated send activity

--- app/src/main/java/com/example/epuser/pickcontacts/SendActivity.java
+++ app/src/main/java/com/example/epuser/pickcontacts/SendActivity.java
@@ -2,12 +2,20 @@ package com.example.epuser.pickcontacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class SendActivity extends AppCompatActivity {
+public class SendActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
