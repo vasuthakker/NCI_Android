87d7de51abc68c251a17aef08bd723b83c10e232
epuser <bittu.dakshana15@gmail.com>
2017-05-26 19:18:36 +0530

    otp verification

--- app/src/main/java/com/example/epuser/pickcontacts/CreatePinFragment.java
+++ app/src/main/java/com/example/epuser/pickcontacts/CreatePinFragment.java
@@ -1,11 +1,78 @@
 package com.example.epuser.pickcontacts;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by epuser on 5/26/2017.
  */
 
 public class CreatePinFragment extends Fragment {
+    private EditText pin_ET , confirm_pin_ET;
+    private Button create_pin_btn;
+    private LoginPage loginActivity;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_create_pin, container, false);
+
+    }
+    private void init() {
+        pin_ET = (EditText)getActivity().findViewById(R.id.pin_ET);
+        confirm_pin_ET = (EditText)getActivity().findViewById(R.id.confirm_pin_ET);
+        create_pin_btn = (Button)getActivity().findViewById(R.id.create_pin_btn);
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        init();
+
+        create_pin_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPin();
+            }
+        });
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        loginActivity = (LoginPage) context;
+    }
+
+    private void createPin() {
+        String pin = pin_ET.getText().toString();
+        if (pin.equals(confirm_pin_ET.getText().toString()))
+        {
+            if(pin.length()==4)
+            {
+                // TODO: 5/26/2017  save in sharedpreferences
+                loginActivity.changeFragment(new LoginFragment());
+
+
+            }
+            else
+            {
+                pin_ET.setError("Please enter a four digit pin");
+            }
+
+        }
+        else
+        {
+            confirm_pin_ET.setError("pins did not match");
+            return;
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/epuser/pickcontacts/LoginPage.java
+++ app/src/main/java/com/example/epuser/pickcontacts/LoginPage.java
@@ -120,15 +120,15 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
             getFragmentManager().popBackStack();
             return;
         }
-        getFragmentManager()
-                .beginTransaction()
-                .setCustomAnimations(
-                        R.animator.card_flip_right_in,
-                        R.animator.card_flip_right_out,
-                        R.animator.card_flip_left_in,
-                        R.animator.card_flip_left_out)
-                .replace(R.id.lgcontainer, new LoginFragment())
-                .commit();
+//        getFragmentManager()
+//                .beginTransaction()
+//                .setCustomAnimations(
+//                        R.animator.card_flip_right_in,
+//                        R.animator.card_flip_right_out,
+//                        R.animator.card_flip_left_in,
+//                        R.animator.card_flip_left_out)
+//                .replace(R.id.lgcontainer, new LoginFragment())
+//                .commit();
     }
 
     protected void isLoggedIn() {
